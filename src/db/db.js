59b1d32d473d3f9ab1a4e360ec77be13643cbe1e
Sobayo <sobayotoby@gmail.com>
2023-09-20 11:47:48 +0100

    fixing undisplayed images

--- src/db/db.js
+++ src/db/db.js
@@ -1,4 +1,16 @@
 import vegetables from './../assets/images/vegetables.jpg'
+import call_of_duty_1 from './../assets/images/call-of-duty-1.jpg'
+import Tacos from './../assets/images/Tacos.webp'
+import car1 from './../assets/images/car-1.jpg'
+import burger_and_pizza from './../assets/images/burger-and-pizza.jpeg'
+import horizon from './../assets/images/horizon.jpg'
+import food_content from './../assets/images/food-content.jpg'
+import joker from './../assets/images/joker.jpg'
+import call_of_duty_2 from './../assets/images/call-of-duty-2.jpg'
+import fruits from './../assets/images/Variety-fruits-vegetables.webp'
+import car2 from './../assets/images/car-2.jpg'
+import god_of_war from './../assets/images/god-of-war.jpg'
+
 
 export const images = [
     {
@@ -9,56 +21,56 @@ export const images = [
     {
       "id": "2",
       "tag": "games",
-      "url": "/src/assets/images/call-of-duty-1.jpg"
+      "url": call_of_duty_1
     },
     {
       "id": "3",
       "tag": "food",
-      "url": "/src/assets/images/Tacos.webp"
+      "url": Tacos
     },
     {
       "id": "4",
       "tag": "cars",
-      "url": "/src/assets/images/car-1.jpg"
+      "url": car1
     },
     {
       "id": "5",
       "tag": "food",
-      "url": "/src/assets/images/burger-and-pizza.jpeg"
+      "url": burger_and_pizza
     },
     {
       "id": "6",
       "tag": "games",
-      "url": "/src/assets/images/horizon.jpg"
+      "url": horizon
     },
     {
       "id": "7",
       "tag": "food",
-      "url": "/src/assets/images/food-content.jpg"
+      "url": food_content
     },
     {
       "id": "8",
       "tag": "movies",
-      "url": "/src/assets/images/joker.jpg"
+      "url": joker
     },
     {
       "id": "9",
       "tag": "games",
-      "url": "/src/assets/images/call-of-duty-2.jpg"
+      "url": call_of_duty_2
     },
     {
       "id": "10",
       "tag": "food",
-      "url": "/src/assets/images/Variety-fruits-vegetables.webp"
+      "url": fruits
     },
     {
       "id": "11",
       "tag": "cars",
-      "url": "/src/assets/images/car-2.jpg"
+      "url": car2
     },
     {
       "id": "12",
       "tag": "games",
-      "url": "/src/assets/images/god-of-war.jpg"
+      "url": god_of_war
     },
   ]
