7bca4fc15b1bf1cce8334da7223a63ca11ff8449
Sobayo <sobayotoby@gmail.com>
2023-09-20 11:01:53 +0100

    fixing undisplayed images

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,4 +1,4 @@
-// import { images } from "../db/db";
+import { images } from "../db/db";
 import classes from "./imageGallery.module.css";
 import SearchBox from "./SearchBox/SearchBox";
 import { useState, useEffect } from "react";
@@ -13,68 +13,7 @@ import { CSS } from "@dnd-kit/utilities";
 import Navbar from "./navbar/Navbar";
 
 
- const images = [
-  {
-    "id": "1",
-    "tag": "food",
-    "url": "/src/assets/images/vegetables.jpg"
-  },
-  {
-    "id": "2",
-    "tag": "games",
-    "url": "/src/assets/images/call-of-duty-1.jpg"
-  },
-  {
-    "id": "3",
-    "tag": "food",
-    "url": "/src/assets/images/Tacos.webp"
-  },
-  {
-    "id": "4",
-    "tag": "cars",
-    "url": "/src/assets/images/car-1.jpg"
-  },
-  {
-    "id": "5",
-    "tag": "food",
-    "url": "/src/assets/images/burger-and-pizza.jpeg"
-  },
-  {
-    "id": "6",
-    "tag": "games",
-    "url": "/src/assets/images/horizon.jpg"
-  },
-  {
-    "id": "7",
-    "tag": "food",
-    "url": "/src/assets/images/food-content.jpg"
-  },
-  {
-    "id": "8",
-    "tag": "movies",
-    "url": "/src/assets/images/joker.jpg"
-  },
-  {
-    "id": "9",
-    "tag": "games",
-    "url": "/src/assets/images/call-of-duty-2.jpg"
-  },
-  {
-    "id": "10",
-    "tag": "food",
-    "url": "/src/assets/images/Variety-fruits-vegetables.webp"
-  },
-  {
-    "id": "11",
-    "tag": "cars",
-    "url": "/src/assets/images/car-2.jpg"
-  },
-  {
-    "id": "12",
-    "tag": "games",
-    "url": "/src/assets/images/god-of-war.jpg"
-  },
-]
+
 
 
 const SortableImages = ({ image }) => {
